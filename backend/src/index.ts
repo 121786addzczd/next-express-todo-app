0213820d28dd22dbe38bbd3590012161182dea72
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-08-30 02:52:40 +0900

    todosのエンドポイント作成

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello express")
+import express from 'express';
+import type { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const PORT = 8000;
+
+app.get('/todos', (req: Request, res: Response) => {
+  const todos = [
+    { id: 1, title: '買い物に行く', completed: false },
+    { id: 2, title: '犬の散歩をする', completed: true },
+    { id: 3, title: '本を読む', completed: false },
+  ];
+
+  return res.json(todos);
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
