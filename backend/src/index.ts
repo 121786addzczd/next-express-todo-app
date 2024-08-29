9eb613bd8893242bd4da6759a5f02dd3119f5de1
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-08-30 03:03:52 +0900

    データベースに設定した属性に合わせてcompletedをisCompletedに変更
    npx prisma studioコマンドでGUIでデータ操作できる

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,9 +6,9 @@ const PORT = 8000;
 
 app.get('/todos', (req: Request, res: Response) => {
   const todos = [
-    { id: 1, title: '買い物に行く', completed: false },
-    { id: 2, title: '犬の散歩をする', completed: true },
-    { id: 3, title: '本を読む', completed: false },
+    { id: 1, title: '買い物に行く', isCompleted: false },
+    { id: 2, title: '犬の散歩をする', isCompleted: true },
+    { id: 3, title: '本を読む', isCompleted: false },
   ];
 
   return res.json(todos);
