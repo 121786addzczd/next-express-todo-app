24d6ae6c2aaeba755b7b6454ad790c290510fe72
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-08-30 03:10:45 +0900

    データベースからToso一覧取得するよう変更

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,15 @@
 import express from 'express';
 import type { Express, Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'; 
 
 const app: Express = express();
 const PORT = 8000;
 
-app.get('/todos', (req: Request, res: Response) => {
-  const todos = [
-    { id: 1, title: '買い物に行く', isCompleted: false },
-    { id: 2, title: '犬の散歩をする', isCompleted: true },
-    { id: 3, title: '本を読む', isCompleted: false },
-  ];
+const prisma = new PrismaClient();
 
-  return res.json(todos);
+app.get('/todos', async (req: Request, res: Response) => {
+  const allTodos = await prisma.todo.findMany();
+  return res.json(allTodos);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
