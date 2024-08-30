4e170e7fb76bad1bb76614f1acd3d2316e91bf9b
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-08-30 18:34:20 +0900

    Todoを作成するAPI追加

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,24 @@ import { PrismaClient } from '@prisma/client';
 const app: Express = express();
 const PORT = 8000;
 
+app.use(express.json());
+
 const prisma = new PrismaClient();
 
 app.get('/todos', async (req: Request, res: Response) => {
   const allTodos = await prisma.todo.findMany();
   return res.json(allTodos);
+});
+
+app.post('/todos', async (req: Request, res: Response) => {
+  const { title, isCompleted } = req.body;
+  const createTodo = await prisma.todo.create({
+    data: {
+      title,
+      isCompleted
+    }
+  });
+  return res.json(createTodo);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
