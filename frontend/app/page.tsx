db8fb2b415065f06af473fdf7e398f1979ff920c
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-08-31 09:28:06 +0900

    useRefで入力したタイトルを取得

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Todo from "./components/Todo";
 import useSWR from "swr";
+import { useRef } from "react";
 import { TodoType } from "./types";
 
 async function fetcher(key: string) {
@@ -9,7 +10,16 @@ async function fetcher(key: string) {
 }
 
 export default function Home() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
   const { data, isLoading, error } = useSWR("http://localhost:8000/todos", fetcher);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (inputRef.current) {
+      console.log(`Addボタン押したらフォームに入力した値が見れる:${inputRef.current.value}`);
+    }
+  }
 
   return (
     <div
@@ -18,7 +28,7 @@ export default function Home() {
   <div className="px-4 py-2">
     <h1 className="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
   </div>
-  <form className="w-full max-w-sm mx-auto px-4 py-2">
+  <form className="w-full max-w-sm mx-auto px-4 py-2" onSubmit={handleSubmit}>
     <div className="flex items-center border-b-2 border-teal-500 py-2">
       <input
         className="appearance-none bg-transparent
@@ -26,6 +36,7 @@ export default function Home() {
       focus:outline-none"
         type="text"
         placeholder="Add a task"
+        ref={inputRef}
       />
       <button
         className="duration-150 flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
